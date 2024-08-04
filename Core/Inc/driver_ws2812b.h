c3c85d4488ab66f7a28d23672262561ec6e14dc1
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-08-04 03:44:56 -0400

    Add initial WS2812B drivers

---
+++ Core/Inc/driver_ws2812b.h
@@ -0,0 +1,72 @@
+/**
+  ******************************************************************************
+  * @file           : driver_ws2812b.c
+  * @brief          : Driver for addressable LED (WS2812B).
+  ******************************************************************************
+  ******************************************************************************
+  * @note:
+  * Based on Phil’s Lab "STM32 + RGB LEDs Firmware Tutorial (TIM + DMA) - Phil's Lab #136" (https://youtu.be/MqbJTj0Cw6o).
+  ******************************************************************************
+  */
+
+#ifndef __DRIVER_WS2812B_H
+#define __DRIVER_WS2812B_H
+
+/*
+ * Includes.
+ */
+
+#include "stm32l4xx_hal.h"
+
+/*
+ * Definitions.
+ */
+
+#define LED_COUNT 5
+
+#define WS2812B_TIM htim1
+#define WS2812B_TIM_CHANNEL TIM_CHANNEL_1
+
+// Based on 80 MHz peripheral clock at 1-1 pre-scaler and 100-1 ARR with time period of 1.25 us.
+#define WS2812B_LO_VAL_DUTY     32  // 0.4 us.
+#define WS2812B_HI_VAL_DUTY     64  // 0.8 us.
+#define WS2812B_RST_VAL_PERIODS 40  // 50 us.
+#define WS2812B_BITS_PER_LED    24  // 8 bytes for R, G, B.
+
+#define WS2812B_DMA_BUF_LEN ((LED_COUNT * WS2812B_BITS_PER_LED) + WS2812B_RST_VAL_PERIODS)
+
+typedef union {
+    struct {
+        // Bit order: G7, G6, ... G0, R7, R6, ... R0, B7, B6, ... B0.
+        uint8_t g;
+        uint8_t r;
+        uint8_t b;
+    } colour;
+
+    uint32_t data;
+
+} WS2812B_LED_DATA_RGB;
+
+/*
+ * Variables.
+ */
+
+extern WS2812B_LED_DATA_RGB WS2812B_LED_DATA[LED_COUNT];
+extern uint8_t WS2812B_DMA_BUF[WS2812B_DMA_BUF_LEN];
+extern volatile uint8_t WS2812B_DMA_COMPLETE_FLAG;
+
+/*
+ * Functions.
+ */
+
+HAL_StatusTypeDef WS2812B_Init();
+
+void WS2812B_Set_Colour(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
+
+HAL_StatusTypeDef WS2812B_Update();
+
+// TODO: Callback for HAL function, call within:
+//  void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim).
+void WS2812B_Callback();
+
+#endif
---
+++ Core/Src/driver_ws2812b.c
@@ -0,0 +1,99 @@
+/**
+  ******************************************************************************
+  * @file           : driver_ws2812b.c
+  * @brief          : Driver for addressable LED (WS2812B).
+  ******************************************************************************
+  * @note:
+  * Based on Phil’s Lab "STM32 + RGB LEDs Firmware Tutorial (TIM + DMA) - Phil's Lab #136" (https://youtu.be/MqbJTj0Cw6o).
+  ******************************************************************************
+  */
+
+/*
+ * Includes.
+ */
+
+#include "driver_ws2812b.h"
+
+/*
+ * Variables.
+ */
+
+extern TIM_HandleTypeDef htim1;
+WS2812B_LED_DATA_RGB WS2812B_LED_DATA[LED_COUNT];
+uint8_t WS2812B_DMA_BUF[WS2812B_DMA_BUF_LEN];
+volatile uint8_t WS2812B_DMA_COMPLETE_FLAG;
+
+/*
+ * Functions.
+ */
+
+HAL_StatusTypeDef WS2812B_Init() {
+  // Initialize PWM timer.
+  HAL_StatusTypeDef hal_status = HAL_TIM_PWM_Init(&WS2812B_TIM);
+
+  // Clear DMA buffer.
+  for (uint16_t buffer_i; buffer_i < WS2812B_DMA_BUF_LEN; buffer_i++) {
+    WS2812B_DMA_BUF[buffer_i] = 0;
+  }
+
+  // Set DMA ready flag.
+  WS2812B_DMA_COMPLETE_FLAG = 1;
+
+  return hal_status;
+}
+
+void WS2812B_Set_Colour(uint8_t index, uint8_t g, uint8_t r, uint8_t b) {
+  WS2812B_LED_DATA[index].colour.r = r;
+  WS2812B_LED_DATA[index].colour.g = g;
+  WS2812B_LED_DATA[index].colour.b = b;
+}
+
+HAL_StatusTypeDef WS2812B_Update() {
+  // Check if previous DMA transfer has been completed.
+  if (!WS2812B_DMA_COMPLETE_FLAG) {
+    return HAL_BUSY;
+  }
+
+  uint16_t buffer_i = 0;
+
+  // For each LED.
+  for (uint8_t led = 0; led < LED_COUNT; led++) {
+
+    // Loop through all 24 data bits.
+    for (uint8_t bits = 0; bits < WS2812B_BITS_PER_LED; bits++, buffer_i++) {
+
+      // Calculate total bit shift.
+      uint8_t byte = (bits / 8) * 8;  // Calculate byte offset as bit count.
+      uint8_t bit = 7 - (bits % 8);  // Calculate bit position.
+      uint8_t bit_index = byte + bit;  // Calculate total bit shift.
+
+      // Update DMA buffer to match LED data.
+      if ((WS2812B_LED_DATA[led].data >> bit_index) & 0x01)  // If bit is set.
+        WS2812B_DMA_BUF[buffer_i] = WS2812B_HI_VAL_DUTY;  // High.
+      else
+        WS2812B_DMA_BUF[buffer_i] = WS2812B_LO_VAL_DUTY;  // Low.
+    }
+
+    // Reset is already set to zero during WS2812B_Init.
+  }
+
+  // Attempt DMA transfer.
+  HAL_StatusTypeDef hal_status = HAL_TIM_PWM_Start_DMA(
+          &WS2812B_TIM,
+          WS2812B_TIM_CHANNEL,
+          (uint32_t *) WS2812B_DMA_BUF,
+          WS2812B_DMA_BUF_LEN
+  );
+
+  // If DMA transfer started successfully.
+  if (hal_status == HAL_OK) {
+    WS2812B_DMA_COMPLETE_FLAG = 0; // Clear DMA flag.
+  }
+}
+
+// TODO: Callback for HAL function, call within:
+//  void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim).
+void WS2812B_Callback() {
+  HAL_TIM_PWM_Stop_DMA(&WS2812B_TIM, WS2812B_TIM_CHANNEL);
+  WS2812B_DMA_COMPLETE_FLAG = 1;
+}
