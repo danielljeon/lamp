8cef4479054fc7dcd37b0e6f68d1ef144b6d2946
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-08-15 19:50:44 -0400

    Add WS2812B driver docs

--- Core/Inc/driver_ws2812b.h
+++ Core/Inc/driver_ws2812b.h
@@ -1,7 +1,7 @@
 /**
  *******************************************************************************
- * @file           : driver_ws2812b.c
- * @brief          : Driver for addressable LED (WS2812B).
+ * @file:  driver_ws2812b.h
+ * @brief: Driver for addressable LED (WS2812B).
  *******************************************************************************
  *******************************************************************************
  * @note:
@@ -38,9 +38,37 @@
 #define WS2812B_DMA_BUF_LEN                                                    \
   ((LED_COUNT * WS2812B_BITS_PER_LED) + WS2812B_RST_VAL_PERIODS)
 
+/**
+ * @brief  Union for representing WS2812B LED color data.
+ *
+ * This union is designed to store and manipulate the color data for a single
+ * WS2812B LED. The color data can be accessed either as individual RGB
+ * components (red, green, blue) or as a combined 32-bit value.
+ *
+ * The structure within the union allows for easy manipulation of the red,
+ * green, and blue color channels individually, while the `data` field provides
+ * a way to handle the entire 24-bit color value as a single unit.
+ *
+ * The bit order in the `data` field is:
+ * - G7, G6, ..., G0 (Green channel, most to least significant bit).
+ * - R7, R6, ..., R0 (Red channel, most to least significant bit).
+ * - B7, B6, ..., B0 (Blue channel, most to least significant bit).
+ *
+ * Usage example:
+ * @code
+ * WS2812B_LED_DATA_RGB led;
+ *
+ * // Set individual color components.
+ * led.colour.r = 255; // Max red intensity.
+ * led.colour.g = 128; // Half green intensity.
+ * led.colour.b = 64;  // Quarter blue intensity.
+ *
+ * // Alternatively, set the entire color using the data field.
+ * led.data = 0xFF8040; // RGB color: (255, 128, 64).
+ * @endcode
+ */
 typedef union {
   struct {
-    // Bit order: G7, G6, ... G0, R7, R6, ... R0, B7, B6, ... B0.
     uint8_t g;
     uint8_t r;
     uint8_t b;
@@ -62,14 +90,49 @@ extern volatile uint8_t WS2812B_DMA_COMPLETE_FLAG;
  * Functions.
  */
 
+/**
+ * @brief  Initializes the WS2812B LED control system.
+ * @retval HAL_StatusTypeDef: Returns the status of the initialization process.
+ */
 HAL_StatusTypeDef WS2812B_Init();
 
+/**
+ * @brief  Sets the color of a specific WS2812B LED.
+ * @param  index: The index of the LED in the array (0-based index).
+ * @param  r: The red component of the color (0-255).
+ * @param  g: The green component of the color (0-255).
+ * @param  b: The blue component of the color (0-255).
+ * @retval None
+ *
+ * Example Usage:
+ * @code
+ * // Set the first LED to bright red.
+ * WS2812B_Set_Colour(0, 255, 0, 0);
+ *
+ * // Set the second LED to green.
+ * WS2812B_Set_Colour(1, 0, 255, 0);
+ * @endcode
+ */
 void WS2812B_Set_Colour(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
 
+/**
+ * @brief  Updates the WS2812B LEDs with the latest color data.
+ * @retval HAL_StatusTypeDef: Returns the status of the operation.
+ */
 HAL_StatusTypeDef WS2812B_Update();
 
-// TODO: Callback for HAL function, call within:
-//  void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim).
+/**
+ * @brief  Callback function to handle the completion of WS2812B data transfer.
+ *
+ * Example usage within `HAL_TIM_PWM_PulseFinishedCallback()`:
+ * @code
+ * void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
+ *   WS2812B_Callback();
+ * }
+ * @endcode
+ *
+ * @retval None
+ */
 void WS2812B_Callback();
 
 #endif
